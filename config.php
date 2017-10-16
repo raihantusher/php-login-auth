d0974a51a493f82ebdc3a8f050ce4cc714b5e69e
raihantusher <raihan.tusher@yahoo.com>
2017-10-16 22:03:54 +0600

    Add files via upload

---
+++ config.php
@@ -0,0 +1,42 @@
+<?php
+
+require("RT_password.php");
+require("RT_session.php");
+//require 'medoo.php';
+class auth{
+
+	public function login($userInfoArr,$password=""){
+            //return boolean type data if password it will true
+            $isPassword=Password::checkPassword($password,$userInfoArr['password']);
+            //if it is true session create
+
+            if($isPassword){
+            	Session::start();
+            	Session::set('user',$userInfoArr);
+            	return true;
+
+            }else {
+            	return false;
+            }
+
+	}// login function finished here
+
+	public function logout(){
+		Session::start();
+	     Session::destroy();
+
+	}//logout function finished here
+    public function getEvr(){
+    	Session::start();
+    	return Session::get('user');
+    }
+}//auth class finished here
+
+//$auth=new auth;
+//$userInfoArr=array(
+ //"username"=>"raihan",
+ //"password"=>'$2y$10$iOJ5i2trZqbvY4KwcOYmTOfE4/k/DL4ydNKMF3ACf1eeqlg.vZNt2'
+//);
+ //$auth->login($userInfoArr,"tusher");
+ //$auth->logout();
+//print_r($auth->getEvr());
