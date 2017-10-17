0820e4829ca9cbd7a546248a556a110f63d767c3
Raihan Tusher <raihan.tusher@yahoo.com>
2017-10-17 11:49:02 +0600

    renamed file

--- index.php
+++ index.php
@@ -1,67 +1,11 @@
 <html>
-
 <head>
- <script src="vue.min.js"></script>   
-   <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-   <script src="https://unpkg.com/vue-router/dist/vue-router.js"></script>
-
-    </head>
+  <title>hello world</title>
+</head>
 
 <body>
-
-<div id="app">
-  <h1>Hello App!</h1>
-  <p>
-    <!-- use router-link component for navigation. -->
-    <!-- specify the link by passing the `to` prop. -->
-    <!-- `<router-link>` will be rendered as an `<a>` tag by default -->
-    <router-link to="/foo">Go to Foo</router-link>
-    <router-link to="/bar">Go to Bar</router-link>
-  </p>
-  <!-- route outlet -->
-  <!-- component matched by the route will render here -->
-  <router-view></router-view>
-</div>
-
-<script type="text/javascript">
-// 0. If using a module system (e.g. via vue-cli), import Vue and VueRouter and then call `Vue.use(VueRouter)`.
-
-// 1. Define route components.
-// These can be imported from other files
-const Foo = { template: '<div><h1>foo</h1></div>' }
-const Bar = { template: '<div>bar</div>' }
-
-// 2. Define some routes
-// Each route should map to a component. The "component" can
-// either be an actual component constructor created via
-// `Vue.extend()`, or just a component options object.
-// We'll talk about nested routes later.
-const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
-]
-
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
-const router = new VueRouter({
-  routes // short for `routes: routes`
-})
-
-// 4. Create and mount the root instance.
-// Make sure to inject the router with the router option to make the
-// whole app router-aware.
-const app = new Vue({
-  router
-}).$mount('#app')
-
-// Now the app has started!
-</script>
-
-
+  
 
 
 </body>
-
-
 </html>
---
+++ session.php
@@ -0,0 +1,80 @@
+<?php
+
+//ff
+class Session{
+
+private static $_sessionStarted=false;
+
+
+
+public static function start(){
+
+  if(self::$_sessionStarted==false)
+{
+
+  session_set_cookie_params("1800","/","",false,true);
+	session_start();
+	self::$_sessionStarted=true;
+}
+
+	}
+
+
+
+
+public static function set($key,$value){
+
+	$_SESSION[$key]=$value;
+}
+
+
+
+
+
+
+
+
+
+public static function get($key,$secondKey=false){
+
+  session_regenerate_id(true);
+   if($secondKey==true){
+           if(isset(($_SESSION[$key][$secondKey])))
+           	return $_SESSION[$key][$secondKey];
+ 
+   }else{
+
+   		if(isset($_SESSION[$key]))
+   			return $_SESSION[$key];
+   }
+
+
+
+
+		return false;
+}
+
+
+
+public static function display(){
+
+	echo '<pre>';
+print_r($_SESSION);
+echo '</pre>';
+
+}
+
+public static function destroy(){
+  if(self::$_sessionStarted==true){
+
+  	session_unset();
+  	session_destroy();
+  }
+
+
+}
+
+
+
+
+}
