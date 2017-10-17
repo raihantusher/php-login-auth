a7f21b82b2adcce3921b7b50f3cfd9e5fafe0419
Raihan Tusher <raihan.tusher@yahoo.com>
2017-10-17 20:37:48 +0600

    update

---
+++ libs/auth.php
@@ -0,0 +1,42 @@
+<?php
+
+require '../config.php';
+//require 'medoo.php';
+class auth{
+
+	public function login($userInfoArr,$password=""){
+            //return boolean type data if password it will true
+            $isPassword=Password::checkPassword($password,$userInfoArr['password']);
+            //if it is true session create
+
+            if($isPassword){
+            	Session::start();
+            	Session::set('user',$userInfoArr);
+            	return true;
+
+            }else {
+            	return false;
+            }
+
+	}// login function finished here
+
+	public function logout(){
+		Session::start();
+	     Session::destroy();
+
+	}//logout function finished here
+    public function getUserDetails(){
+    	Session::start();
+    	return Session::get('user');
+    }
+}//auth class finished here
+
+$auth=new auth;
+$userInfoArr=array(
+ "username"=>"raihan",
+ "password"=>'$2y$10$iOJ5i2trZqbvY4KwcOYmTOfE4/k/DL4ydNKMF3ACf1eeqlg.vZNt2'
+);
+ 
+ $auth->login($userInfoArr,"tusher");
+ //$auth->logout();
+print_r($auth->getUserDetails());
